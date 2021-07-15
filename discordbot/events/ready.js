d8821ab44d69b30579fee4ac754438ce3c578b41
yuvapoojary <yuvapoojaryk.kone@gmail.com>
2021-07-15 16:48:12 +0530

    forgot to change bot presence config key

--- discordbot/events/ready.js
+++ discordbot/events/ready.js
@@ -2,7 +2,7 @@
 module.exports = client => {
   
     console.log(`Connected to ${client.guilds.cache.size} guilds`)
-    client.user.setPresence({ game: client.config.bot.presence });  
+    client.user.setPresence({ game: client.config.bot_presence });  
     
 };
   
