1ca0904e889391d76550278e4b55b8c8f8c6a018
yuvapoojary <yuvapoojaryk.kone@gmail.com>
2021-07-15 16:38:58 +0530

    gg

--- config.js
+++ config.js
@@ -1,47 +1,50 @@
 require('dotenv').config();
 
 const config = {
-  
+
   port: 5001,
-  
+
   secret: 'some string',
-  
+
   mongoURL: '',
-  
+
   bot_token: '',
-  
+
   bot_prefix: '.',
-  
+
   bot_clientId: '',
-  
+
   bot_clientSecret: '',
-  
+
   bot_callbackURL: '',
-  
+
   bot_presence: {
     name: 'discord bots',
     type: 3
   },
-  
+
   guildId: '',
-  
+
   perms: {
     owners: ['572327928646598667'],
     approvers: []
   },
-  
+
   roles: {
-    
+
   }
-  
+
 };
 
 const mergeConfig = (data) => {
+  console.log(process.env.bot_token)
+  const newConfig = {};
   const keys = Object.keys(data);
-  for(const key in keys) {
-    if(process.env[key]) data[key] = process.env[key];
+  for (const key in keys) {
+    const value = process.env[key];
+    newConfig[key] = value || data[key];
   };
-  return data;
+  return newConfig;
 };
 
 module.exports = mergeConfig(config);
