14f8d11609e6f98f53298184250fffe0a0e2ae54
yuvapoojary <yuvapoojaryk.kone@gmail.com>
2021-07-15 16:59:09 +0530

    fix typo

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const minifyHTML = require("express-minify-html");
 const helmet = require('helmet');
 const client = require('./bot');
 
-const port = rocess.env.PORT || config.port;
+const port = process.env.PORT || config.port;
 global.client = client;
 
 const app = express();
